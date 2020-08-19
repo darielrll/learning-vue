03cc898848b4f585f06bfd47e6bae3d3f8a9236d
Dariel Rivero LLerena <dariel@Dariels-MacBook-Pro.local>
2020-08-19 00:39:08 -0400

    Convertir los bloques de secciones en componente, me quedé en 'Anidar componentes con Vue.JS'

--- vue-09/bloques-a-componentes/src/App.vue
+++ vue-09/bloques-a-componentes/src/App.vue
@@ -4,26 +4,10 @@
 
     <div class="container">
       <div class="row">
-        <ul class="nav">
-          <li class="nav-item">
-            <a class="nav-link active" href="#">Active</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-          </li>
-        </ul>
+        <Navegacion/>
       </div>
       <div class="row">
-        <h1>Mi web</h1>
-        <p>
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        </p>
+        <Contenido/>
       </div>
     </div>
   </div>
@@ -31,11 +15,14 @@
 
 <script>
 import MiComponente from './components/MiComponente.vue'
+import Navegacion from './components/Navegacion.vue'
+import Contenido from './components/Contenido.vue'
 
 export default {
   name: 'App',
   components: {
-    MiComponente
+    Contenido,
+    MiComponente, Navegacion
   }
 }
 </script>
---
+++ vue-09/bloques-a-componentes/src/components/Navegacion.vue
@@ -0,0 +1,34 @@
+<template>
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link" v-bind:class="{ active : mostrarActivo }" href="#">Active</a>
+        </li>
+        <li class="nav-item" v-bind:style="{backgroundColor: colorRandom, border: 'solid 3px yellow'}">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+        </ul>
+</template>
+
+<script>
+export default {
+    name: 'Navegacion',
+    data() {
+        return {
+            mostrarActivo: true,
+            colorRandom: "#" + Math.floor(Math.random() * 6777215).toString(16)
+        }
+    },
+}
+</script>
+
+<style>
+    .active {
+        font-weight: bold;
+    }
+</style>
