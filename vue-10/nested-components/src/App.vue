7781739651219dbe843ce0ff8c7786f74f3968c6
Dariel Rivero LLerena <dariel@Dariels-MacBook-Pro.local>
2020-08-20 23:35:42 -0400

    Created nested components project

---
+++ vue-10/nested-components/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <Navegacion/>
+      </div>
+      
+      <div class="row">
+        <Contenido/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navegacion from './components/Navegacion.vue'
+import Contenido from './components/Contenido.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Contenido, Navegacion
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ vue-10/nested-components/src/components/MiComponente.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        dddjjj
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="">
+
+</style>
+
