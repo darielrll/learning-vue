008c3dc71fe5620f2c985bc7bc7271e29a024c9c
Dariel Rivero LLerena <dariel@Dariels-MacBook-Pro.local>
2020-08-20 23:45:20 -0400

    End of nested components

--- vue-10/nested-components/src/components/Navegacion.vue
+++ vue-10/nested-components/src/components/Navegacion.vue
@@ -1,30 +1,20 @@
 <template>
-    <ul class="nav">
-        <li class="nav-item">
-            <a class="nav-link" v-bind:class="{ active : mostrarActivo }" href="#">Active</a>
-        </li>
-        <li class="nav-item" v-bind:style="{backgroundColor: colorRandom, border: 'solid 3px yellow'}">
-            <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-        </li>
-        
-        <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
+    <ul class="nav">        
+        <NavegacionItem />
+        <NavegacionItem />
+        <NavegacionItem />
+        <NavegacionItem />
     </ul>
 </template>
 
 <script>
+import NavegacionItem from './NavegacionItem.vue'
+
 export default {
     name: 'Navegacion',
-    data() {
-        return {
-            mostrarActivo: true,
-            colorRandom: "#" + Math.floor(Math.random() * 6777215).toString(16)
-        }
-    },
+    components: {
+        NavegacionItem
+    }
 }
 </script>
 
---
+++ vue-10/nested-components/src/components/NavegacionItem.vue
@@ -0,0 +1,23 @@
+<template>
+    
+    <li class="nav-item" v-bind:style="{backgroundColor: colorRandom, border: 'solid 3px yellow'}">
+        <a class="nav-link" href="#">Link</a>
+    </li>
+    
+</template>
+
+<script>
+export default {
+    name: "NavegacionItem",
+    data() {
+        return {
+            mostrarActivo: true,
+            colorRandom: "#" + Math.floor(Math.random() * 6777215).toString(16)
+        }
+    }
+}
+</script>
+
+<style lang="">
+    
+</style>
