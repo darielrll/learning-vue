3c49a800b8d195e37a127ab8e157e228a9317961
Dariel Rivero LLerena <dariel@Dariels-MacBook-Pro.local>
2020-08-22 17:44:09 -0400

    Methods with paramaters

--- vue-09/bloques-a-componentes/src/components/Opcion1.vue
+++ vue-09/bloques-a-componentes/src/components/Opcion1.vue
@@ -2,13 +2,14 @@
     <div class="card col-md-4">
         <div class="card-body">
             <h5 class="card-title">
-                Opcion 1 {{opcion}}
+                {{texto}}
             </h5>
             <p class="card-text">
                 "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium."
             </p>
 
-            <button class="btn btn-primary" v-on:click="sumar">Opcion 1</button>
+            <button class="btn btn-primary" v-on:click="letraElegida('A')">Boton A</button> <br/><br/>
+            <button class="btn btn-primary" v-on:click="letraElegida('B')">Boton B</button>
         </div>
     </div>
 </template>
@@ -18,12 +19,16 @@ export default {
     name: "Opcion1",
     data() {
         return {
-            opcion: "xxx"
+            opcion: "xxx",
+            texto: "Titular"
         }
     },
     methods: {
         sumar: function() {
             this.opcion = Math.floor(Math.random() * 1000)
+        },
+        letraElegida: function (letra) {
+            this.texto = "Seleccionaste el boton " + letra
         }
     }
 }
